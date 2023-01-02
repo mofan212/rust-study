9d5a82bc9ad61e7adc3990c9e4e01f7f28aa613f
mofan <cy.mofan@foxmail.com>
2023-01-02 17:39:42 +0800

    guess number

--- src/guess_number.rs
+++ src/guess_number.rs
@@ -1,30 +1,38 @@
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io; // prelude // trait
+use std::io;
 
-fn main() {
-    println!("猜数！");
+fn guess_number() {
+    println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("神秘数字是：{}", secret_number);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {}", secret_number);
 
     loop {
-        println!("猜测一个数");
+        println!("Please input your guess");
 
         let mut guess = String::new();
-        // io::Result Ok, Error
-        io::stdin().read_line(&mut guess).expect("无法读取行");
-        // shadow
-        let guess:u32 = match guess.trim().parse() {
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // 假设用户输入了 5，需要先 trim() 下，因为用户输入的内容可能像 5\n 或者 5\r\n
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
+            // 匹配所有 Err 值
             Err(_) => continue,
         };
-        println!("你猜测的数是：{}", guess);
+
+        // println!("You guessed: {}", guess);
+
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too Small!"), // arm
-            Ordering::Greater => println!("Too Big!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You Win!");
+                println!("You win!");
+                // 数字猜对就退出
                 break;
             }
         }
