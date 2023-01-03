d1eed666bec5b278f81b6041f9a0d2d6f05dc81d
mofan <cy.mofan@foxmail.com>
2023-01-03 21:02:21 +0800

    什么是所有权

--- src/ownership/ownership.rs
+++ src/ownership/ownership.rs
@@ -8,3 +8,48 @@ fn string_test () {
     let s2 = s1.clone();
     println!("s1 = {}, s2 = {}", s1, s2);
 }
+
+
+fn function() {
+    let s = String::from("hello");
+    takes_ownership(s);
+
+    let x = 5;
+    makes_copy(x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+fn return_ownership() {
+    let s1 = gives_ownership();
+    println!("{s1}");
+
+    let s2 = String::from("hello");
+
+    let s3 = takes_and_gives_back(s2);
+    println!("{s3}");
+
+    let s4 = String::from("hello");
+    let (s5, len) = calculate_length(s4);
+    println!("The length of '{}' is {}", s5, len);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
+}
